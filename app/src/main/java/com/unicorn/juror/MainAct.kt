f71124d3bbe33d78c7b81b3a182eddf6c12bb3fb
ivotai <ivotai@163.com>
2018-03-14 10:19:28 +0800

    添加依赖

--- app/src/main/java/com/unicorn/juror/MainAct.kt
+++ app/src/main/java/com/unicorn/juror/MainAct.kt
@@ -1,18 +1,12 @@
 package com.unicorn.juror
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginAct : AppCompatActivity() {
+class MainAct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_login)
-
-        Intent(this,MainAct::class.java).apply{
-            startActivity(this)
-        }
+        setContentView(R.layout.activity_main)
     }
-
 }
