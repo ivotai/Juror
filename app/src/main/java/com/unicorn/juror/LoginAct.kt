55f4c4abe014bfd51082273bb5a4695bd9499b3d
ivotai <ivotai@163.com>
2018-03-14 10:12:12 +0800

    Initial commit

---
+++ app/src/main/java/com/unicorn/juror/LoginAct.kt
@@ -0,0 +1,13 @@
+package com.unicorn.juror
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_login)
+    }
+
+}
