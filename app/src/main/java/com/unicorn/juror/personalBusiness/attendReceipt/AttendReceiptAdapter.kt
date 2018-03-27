9c7c4cb0dce04460b75154c9d0501abff35dda4b
ivotai <ivotai@163.com>
2018-03-27 20:17:04 +0800

    界面调整

--- app/src/main/java/com/unicorn/juror/personalBusiness/attendReceipt/AttendReceiptAdapter.kt
+++ app/src/main/java/com/unicorn/juror/personalBusiness/attendReceipt/AttendReceiptAdapter.kt
@@ -1,6 +1,7 @@
 package com.unicorn.juror.personalBusiness.attendReceipt
 
 import android.annotation.SuppressLint
+import com.afollestad.materialdialogs.DialogAction
 import com.afollestad.materialdialogs.MaterialDialog
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
@@ -10,18 +11,40 @@ import com.unicorn.juror.app.default
 import com.unicorn.juror.dagger.ComponentHolder
 import com.unicorn.juror.personalBusiness.model.Bs
 import com.unicorn.juror.util.DialogUtils
-import org.joda.time.DateTime
 
 class AttendReceiptAdapter : BaseQuickAdapter<Bs, BaseViewHolder>(R.layout.item_attend_receipt) {
 
     override fun convert(helper: BaseViewHolder, item: Bs) {
         helper.apply {
-            setText(R.id.tvAhqc, "案号全称:${item.ahqc}")
-            setText(R.id.tvSqrmc, "申请人:${item.sqrmc}")
-            setText(R.id.tvSqsj, "申请时间:${DateTime(item.sqtime).toString("yyyy-MM-dd")}")
+            //            setText(R.id.tvAhqc, "案号全称:${item.ahqc}")
+//            setText(R.id.tvSqrmc, "申请人:${item.sqrmc}")
+//            setText(R.id.tvSqsj, "申请时间:${DateTime(item.sqtime).toString("yyyy-MM-dd")}")
             // 5确认出庭
-            setOnClickListener(R.id.btnAgree, { replyAttend(5, item) })
-            setOnClickListener(R.id.btnDisagree, { replyAttend(4, item) })
+            setOnClickListener(R.id.btnAgree, {
+                //                replyAttend(5, item) }
+                MaterialDialog.Builder(mContext)
+                        .title("确认出庭")
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onPositive(object : MaterialDialog.SingleButtonCallback {
+                            override fun onClick(dialog: MaterialDialog, which: DialogAction) {
+                                replyAttend(5, item)
+                            }
+                        }).show()
+
+            })
+
+            setOnClickListener(R.id.btnDisagree, {
+                MaterialDialog.Builder(mContext)
+                        .title("确认不出庭")
+                        .positiveText("确认")
+                        .negativeText("取消")
+                        .onPositive(object : MaterialDialog.SingleButtonCallback {
+                            override fun onClick(dialog: MaterialDialog, which: DialogAction) {
+                                replyAttend(4, item)
+                            }
+                        }).show()
+            })
         }
     }
 
@@ -48,5 +71,6 @@ class AttendReceiptAdapter : BaseQuickAdapter<Bs, BaseViewHolder>(R.layout.item_
                     }
                 }
     }
+
 
 }
